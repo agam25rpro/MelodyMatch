52407f7641bb4a77b21d208fbe1cbed048e5f3e6
Agam <agam25r@gmail.com>
2024-12-26 16:03:28 +0530

    first commit

---
+++ appgood.py
@@ -0,0 +1,56 @@
+import pickle
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+from flask import Flask, render_template, request, jsonify
+
+# Initialize Flask app
+app = Flask(__name__)
+
+# Spotify credentials
+CLIENT_ID = "0c53205e120e40e08250e16d31bd15f7"
+CLIENT_SECRET = "6157ff0983dc486eb4b46d92751cc7b6"
+
+# Initialize the Spotify client
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+# Load data
+music = pickle.load(open('df.pkl', 'rb'))
+similarity = pickle.load(open('similarity.pkl', 'rb'))
+
+def get_song_album_cover_url(song_name, artist_name):
+    search_query = f"track:{song_name} artist:{artist_name}"
+    results = sp.search(q=search_query, type="track")
+
+    if results and results["tracks"]["items"]:
+        track = results["tracks"]["items"][0]
+        album_cover_url = track["album"]["images"][0]["url"]
+        return album_cover_url
+    else:
+        return "https://i.postimg.cc/0QNxYz4V/social.png"
+
+def recommend(song):
+    index = music[music['song'] == song].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_music_names = []
+    recommended_music_posters = []
+    for i in distances[1:6]:
+        artist = music.iloc[i[0]].artist
+        recommended_music_posters.append(get_song_album_cover_url(music.iloc[i[0]].song, artist))
+        recommended_music_names.append(music.iloc[i[0]].song)
+
+    return recommended_music_names, recommended_music_posters
+
+@app.route('/')
+def index():
+    music_list = music['song'].values
+    return render_template('index.html', music_list=music_list)
+
+@app.route('/recommend', methods=['POST'])
+def recommend_song():
+    song = request.form['song']
+    recommended_music_names, recommended_music_posters = recommend(song)
+    return jsonify(names=recommended_music_names, posters=recommended_music_posters)
+
+if __name__ == '__main__':
+    app.run(debug=True)
